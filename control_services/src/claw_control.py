4b3dd1d8f4cd9de6ea23b5dda69b1b3305d9903d
Cleber Couto Filho <clebercoutof@gmail.com>
2018-10-09 14:17:08 -0300

    test

--- control_services/src/claw_control.py
+++ control_services/src/claw_control.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_srvs.msg  import Float64
+from std_msgs.msg  import Float64
 from std_srvs.srv import Empty
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
@@ -8,7 +8,7 @@ F_CLAW_JOINTS = ['claw_f1','claw_f2']
 B_CLAW_JOINTS = ['claw_b1','claw_b2']
 AP_CLAW_JOINTS = ['claw_ap']
 
-OPEN_Q = [1.57,-1.57]
+OPEN_Q = [-1.57,1.57]
 CLOSE_Q = [0, 0]
 
 class claw_control_services():
@@ -17,32 +17,48 @@ class claw_control_services():
         #Creating our node,publisher and subscriber
         self.open_f_claw_service = rospy.Service('f_claw/open', Empty, self.open_f_claw)
         self.close_f_claw_service = rospy.Service('f_claw/close', Empty, self.close_f_claw)
-        self.f_claw_publisher = rospy.Publisher('/JVRC_1/highgain_control/set_joint_trajectory', JointTrajectory, queue_size=10)  
+        self.f_claw_publisher = rospy.Publisher('/f_claw_trajectory_controller/command', JointTrajectory, queue_size=10)  
 
-        self.open_b_claw_service = rospy.Service('b_claw/open', Empty, self.open_b_claw)
-        self.close_b_claw_service = rospy.Service('b_claw/close', Empty, self.close_b_claw)
+        #self.open_b_claw_service = rospy.Service('b_claw/open', Empty, self.open_b_claw)
+        #self.close_b_claw_service = rospy.Service('b_claw/close', Empty, self.close_b_claw)
 
-        self.open_ap_claw_service = rospy.Service('ap_claw/open', Empty, self.open_ap_claw)
-        self.close_ap_claw_service = rospy.Service('ap_claw/close', Empty, self.close_ap_claw)
+        #self.open_ap_claw_service = rospy.Service('ap_claw/open', Empty, self.open_ap_claw)
+        #self.close_ap_claw_service = rospy.Service('ap_claw/close', Empty, self.close_ap_claw)
 
         rospy.spin()
         
     #Callback function implementing the pose value received
-    def open_f_claw(self):
+    def open_f_claw(self,req):
         #Get current time for header stamp
         time = rospy.get_time()
         #Joint Trajectory message
         msg = JointTrajectory()
-        msg.header.time = time
-        msg.joint_names = F_CLAW_JOINT
+        msg.joint_names = F_CLAW_JOINTS
+        msg.points = []
         #Joint Trajectory points
         points = JointTrajectoryPoint()
         points.time_from_start = rospy.rostime.Duration(2)
         points.positions = OPEN_Q
 
-        msg.points = points
-        self.f_claw_publisher(msg)
+        msg.points.append(points)
+        self.f_claw_publisher.publish(msg)
         rospy.loginfo("f_claw Opened")
+
+    def close_f_claw(self,req):
+        #Get current time for header stamp
+        time = rospy.get_time()
+        #Joint Trajectory message
+        msg = JointTrajectory()
+        msg.joint_names = F_CLAW_JOINTS
+        msg.points = []
+        #Joint Trajectory points
+        points = JointTrajectoryPoint()
+        points.time_from_start = rospy.rostime.Duration(2)
+        points.positions = CLOSE_Q
+
+        msg.points.append(points)
+        self.f_claw_publisher.publish(msg)
+        rospy.loginfo("f_claw CLOSED")
 
 if __name__ == '__main__':
     try:
