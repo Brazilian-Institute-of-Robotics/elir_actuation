08c8daad96764fea975e420d8b87ebe5fd064a86
Cleber Couto Filho <clebercoutof@gmail.com>
2018-11-09 13:15:14 -0300

    add joy_control

--- joy_control/src/joy_control.py
+++ joy_control/src/joy_control.py
@@ -9,8 +9,6 @@ from sensor_msgs.msg import JointState, Joy
 class joy_control():
 
     def __init__(self):
-        #Creating our node,publisher and subscriber
-        self.b_arm_subscriber = rospy.Subscriber('elir/joint_states', JointState, self.callback)
 
         self.joy_subscriber = rospy.Subscriber('/joy', Joy, self.joy_callback)
         self.joint1f_publisher = rospy.Publisher('/joint1_f_controller/command', Float64, queue_size=10)
@@ -39,16 +37,17 @@ class joy_control():
         rospy.spin()
 
     #Callback function implementing the pose value received
-    def callback_join1f(self,data):
+    def callback_joint1f(self,data):
         self.current_joint1f = data.current_pos
-    def callback_join1b(self,data):
+    def callback_joint1b(self,data):
         self.current_joint1b = data.current_pos
-    def callback_join2f(self,data):
+    def callback_joint2f(self,data):
         self.current_joint2f = data.current_pos
-    def callback_join2b(self,data):
+    def callback_joint2b(self,data):
         self.current_joint2b = data.current_pos
 
     def joy_callback(self,data):
+
         left_horizontal_analog = data.axes[0]
         left_vertical_analog = data.axes[1]
 
@@ -60,11 +59,15 @@ class joy_control():
 
         if left_horizontal_analog != 0 :
             if left_horizontal_analog > 0.8:
-                data = 1
-                self.line_horizontal_control(data)
-            if left_horizontal_analog < -0.8:
                 data = -1
                 self.line_horizontal_control(data)
+                print("esquerda")
+            if left_horizontal_analog < -0.8:
+                data = 1
+                self.line_horizontal_control(data)
+                print("direita")
+        else:
+            self.line_horizontal_control(0)
     
         # if right_vertical_analog != 0:
         #     if right_vertical_analog > 0.8:
@@ -74,46 +77,14 @@ class joy_control():
         #         data = 1
         #         self.arm_move(data)
 
-        if data.button_analog_up or data.button_analog_down:
+        if button_analog_up or button_analog_down:
+            print("botao")
             if data.button_analog_up > 0:
                 data = -1
                 line_push_up(data)
             if data.button_analog_down > 0:
                 data = 1
                 line_push_up(data)
-
-    def arm_move(self, data):
-        key_vel = 3*data
-        #F_ARM
-        self.UP_F_ARM_POINTS = [ key_vel + self.current_joint1f , kel_vel +self.current_joint2f]
-        time = rospy.get_time()
-        #Joint Trajectory message
-        msg = JointTrajectory()
-        msg.joint_names = self.F_ARM_JOINTS
-        msg.points = []
-        #Joint Trajectory points
-        points = JointTrajectoryPoint()
-        points.time_from_start = self.trajectory_duration
-        points.positions = self.UP_F_ARM_POINTS
-        #Append the trajectory points and publish
-        msg.points.append(points)
-        self.f_arm_publisher.publish(msg)
-
-        #B_ARM
-        self.UP_B_ARM_POINTS = [key_vel + self.current_joint1b , key_vel + self.current_joint2b]
-        #Get current time for header stamp
-        time = rospy.get_time()
-        #Joint Trajectory message
-        msg = JointTrajectory()
-        msg.joint_names = self.B_ARM_JOINTS
-        msg.points = []
-        #Joint Trajectory points
-        points = JointTrajectoryPoint()
-        points.time_from_start = self.trajectory_duration
-        points.positions = self.UP_B_ARM_POINTS
-        #Append the trajectory points and publish
-        msg.points.append(points)
-        self.b_arm_publisher.publish(msg)
 
     def traction_control(self, data):
         key_vel = 3*data.angular.z
@@ -125,9 +96,14 @@ class joy_control():
         goal_joint1f = self.joint1_f + 0.1*data
         self.joint1f_publisher.publish(goal_joint1f)
         self.rate.sleep()
-        
+    
+    def line_horizontal_control(self, data):
+        key_vel = 3*data
+        self.traction_f_publisher.publish(key_vel)
+        self.traction_b_publisher.publish(key_vel)
+        self.traction_ap_publisher.publish(-key_vel)
 if __name__ == '__main__':
     try:
         rospy.init_node('joy_controller', anonymous=True)
-        x = arm_up_down_mimic_control()
+        x = joy_control()
     except rospy.ROSInterruptException: pass
